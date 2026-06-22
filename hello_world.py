d2d87bfb23ffcf40311d1fcbb39c02c2c4e9e145
thealeemquadri <mohammedaleem061@gmail.com>
2026-06-21 21:44:30 -0500

    Initial commit: Hello World

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+"""
+CS 898BA: Image Analysis and Computer Vision
+Course Project: Diabetic Retinopathy Severity Grading from Retinal Fundus Images
+Author: Abdul Aleem Mohammed
+Wichita State University
+
+Initial "Hello World!" commit for the Project Pitch milestone.
+"""
+
+
+def main():
+    print("Hello World!")
+    print("CS 898BA Project: Diabetic Retinopathy Severity Grading")
+    print("Author: Abdul Aleem Mohammed")
+
+
+if __name__ == "__main__":
+    main()
